563b95e1eb1b0651a64bd7d15bf1dfb5a315a03d
Nick Haddock <nick.haddock@pearson.com>
2016-04-10 14:21:37 +0100

    Minor updates to startup script for clarity

--- sainsburytest.sh
+++ sainsburytest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm app-servers web-servers 
 # Instantiate cloud instances using Ruby scripts
-./createdroplet.rb -n app
-./createdroplet.rb -n app2
-./createdroplet.rb -n web
+./createdroplet.rb -n appserv
+./createdroplet.rb -n 2ndappserv
+./createdroplet.rb -n webserver
 echo "Waiting for VM's to fully start"
-sleep 30
+sleep 40
 ./lstdroplets.rb
 ./lstdroplets.rb | grep app | awk '{print $8}' >> app-servers
 ./lstdroplets.rb | grep web | awk '{print $8}' >> web-servers
@@ -15,10 +15,10 @@ count=1
 for i in `cat app-servers`
 do
         IP[$count]=${i}
-	scp -o StrictHostKeyChecking=no app-install.sh root@$i:~ 
-	scp -o StrictHostKeyChecking=no app-chef-code.tar root@$i:~
-	scp -o StrictHostKeyChecking=no go-code.go root@$i:~
-	ssh -o StrictHostKeyChecking=no root@$i -C "chmod +x /root/app-install.sh ; /root/app-install.sh > install-log 2>1&"
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no app-install.sh root@$i:~ 
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no app-chef-code.tar root@$i:~
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no go-code.go root@$i:~
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i -C "chmod +x /root/app-install.sh ; /root/app-install.sh > install-log 2>1&"
        	#ssh -o StrictHostKeyChecking=no root@$i
 	((count+=1))
 done
@@ -40,15 +40,15 @@ server {
 EOF
 for i in `cat web-servers`
 do
-	scp -o StrictHostKeyChecking=no web-install.sh root@$i:~ 
-	scp -o StrictHostKeyChecking=no web-chef-code.tar root@$i:~
-	scp -o StrictHostKeyChecking=no load-balancer.conf root@$i:~
-	ssh -o StrictHostKeyChecking=no root@$i -C "chmod +x /root/web-install.sh ; /root/web-install.sh > install-log 2>1&"
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no web-install.sh root@$i:~ 
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no web-chef-code.tar root@$i:~
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no load-balancer.conf root@$i:~
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i -C "chmod +x /root/web-install.sh ; /root/web-install.sh > install-log 2>1&"
        	#ssh -o StrictHostKeyChecking=no root@$i
 done
 echo "Waiting for Web server code to launch and set up Load Balancer"
 echo "This can take up to a minute at slow times on the service - so please be patient"
-sleep 60
+sleep 90
 echo "**************************************"
 echo "IP address of web server to test `cat web-servers`"
 echo "**************************************"
